d38fcad8c3beeb8cabf04a40ec151b58b21862de
Brian <karanjabrian001@gmail.com>
2024-07-15 23:27:20 +0300

    Improved blade view for web3defi

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,93 +13,137 @@
         <!-- Styles -->
         @vite('resources/css/app.css')
     </head>
-    <body class="font-sans antialiased dark:bg-gray-300 dark:text-black">
+    <body class="font-sans antialiased dark:bg-slate-700 dark:text-white">
         <div class="grid grid-cols-12 grid-height max-w-7xl gap-2 mx-auto mt-10">
-            <div class="bg-white col-span-2 row-span-6 shadow-lg">
+            <div class="bg-slate-700 col-span-2 row-span-6 shadow-lg">
                 <nav class="grid p-4 text-center ">
-                    <div class="mb-4 mt-4 bg-white py-4 hover:drop-shadow-md cursor-pointer">
-                        <a href="#" class="">Home</a>
+                    <div class="mb-4 mt-4 bg-slate-600 py-4 hover:drop-shadow-md cursor-pointer">
+                        <a href="#" class="hover:text-cyan-300">Home</a>
                     </div>
-                    <div class="mb-4 bg-white py-4 hover:drop-shadow-md">
-                        <a href="#" class="">Jobs</a>
+                    <div class="mb-4 bg-slate-600 py-4 hover:drop-shadow-md">
+                        <a href="#" class="hover:text-cyan-300">Jobs</a>
                     </div>
-                    <div class="mb-4 bg-white py-4 hover:drop-shadow-md">
-                        <a href="#" class="">About</a>
+                    <div class="mb-4 bg-slate-600 py-4 hover:drop-shadow-md">
+                        <a href="#" class="hover:text-cyan-300">About</a>
                     </div>
-                    <div class="mb-4 bg-white py-4 hover:drop-shadow-md">
-                        <a href="#" class="">Contact</a>
+                    <div class="mb-4 bg-slate-600 py-4 hover:drop-shadow-md">
+                        <a href="#" class="hover:text-cyan-300">Contact</a>
                     </div>
 
                 <nav>
             </div>
 
-            <div class="bg-white col-span-7 row-span-6 shadow-lg">
-                <h1 class="text-3xl text-cyan-600 p-4 text-center">Web3Defi</h1>
-                <p class="p-4 -tracking-wider">
-                    Lorem is simply dummy text of the printing and typesetting industry. 
-                    Lorem Ipsum has been the industry's standard dummy text ever 
-                    since the 1500s, when an unknown printer took a galley of type
-                    and scrambled it to make a type specimen book.
-                </p>
-                <p class="p-4 -tracking-wider">
-                    Lorem is simply dummy text of the printing and typesetting industry. 
-                    Lorem Ipsum has been the industry's standard dummy text ever 
-                    since the 1500s, when an unknown printer took a galley of type
-                    and scrambled it to make a type specimen book.
-                </p>
-                <p class="p-4 -tracking-wider">
-                    Lorem is simply dummy text of the printing and typesetting industry. 
-                    Lorem Ipsum has been the industry's standard dummy text ever 
-                    since the 1500s, when an unknown printer took a galley of type
-                    and scrambled it to make a type specimen book.
-                </p>
-            </div>
-            <div class="bg-white col-span-3 row-span-6 shadow-lg">
-                Three
-            </div>
-            <div class="bg-white grid-search-box shadow-lg mb-2">
-                <input type="search" class="grid-search-input">
-            </div>
-        </div>
-
-        <div class="grid grid-cols-12 grid-height max-w-7xl gap-4 mx-auto mb-6">
-            <div class="bg-white col-span-5 row-span-3">
-                <div class="grid grid-cols-2 gap-2">
-                    <p class=" grid justify-center items-center"> 
-                    lorem when an unknown printer took a galley of type
-                    and scrambled it to make a type specimen book
+            <div class=" col-span-7 row-span-6 shadow-lg rounded-md relative">
+                <div class="relative">
+                    <img src="{{asset('images/web3v5.webp')}}" class="w-full h-full object-cover rounded-md" alt="Photo">
+                </div>
+                <div class="absolute inset-0 bg-gradient-to-b from-transparent to-slate-900/90 flex flex-col justify-center">
+                    <h1 class="text-3xl text-cyan-300 p-4 text-center font-bold">Web3Defi</h1>
+                    <p class="p-4 tracking-wider">
+                        Lorem is simply dummy text of the printing and typesetting industry. 
+                        Lorem Ipsum has been the industry's standard dummy text ever 
+                        since the 1500s, when an unknown printer took a galley of type
+                        and scrambled it to make a type specimen book.
                     </p>
                 </div>
             </div>
             
-            <div class="bg-white col-span-5 row-span-3 p-4 flex flex-col">
-                 <div class="flex">
-                    <img src="path/to/photo.jpg" class="w-1/4 h-full object-cover" alt="Photo">
-                    <div class="ml-4 flex-1">
-                        <h2 class="text-xl font-bold">Title Here</h2>
-                        <p class="text-gray-700">
+            <div class="relative col-span-3 row-span-6 shadow-lg rounded-md">
+                <div class="absolute inset-0 bg-gradient-to-b from-transparent to-slate-900/90 rounded-md"></div>
+                <img src="{{asset('images/block2.png')}}" class="w-full h-full object-cover rounded-md" alt="Photo">
+            </div>
+            <div class="bg-slate-600 grid-search-box shadow-lg mb-2">
+                <input type="search" class="grid-search-input bg-slate-600">
+            </div>
+        </div>
+
+        <div class="grid grid-cols-12 grid-height max-w-7xl gap-4 mx-auto mb-6">
+            <div class="bg-slate-600 col-span-5 row-span-3 flex flex-col rounded-tl-3xl rounded-bl-3xl rounded-r-md shadow-slate-900 drop-shadow-sm hover:drop-shadow-md">
+                <div class="flex">
+                   <img src="{{asset('images/logo5.jpg')}}" class="w-1/4 h-full object-cover rounded-tl-3xl rounded-bl-3xl" alt="Photo">
+                   <div class="ml-4 flex-1 p-8">
+                       <h2 class="text-xl font-bold mb-4 text-cyan-300">Title One</h2>
+                       <p>
+                           This is a small paragraph below the title.
+                           Lorem is simply dummy text of the printing and typesetting industry. 
+                           Lorem Ipsum has been the industry's standard dummy text ever 
+                           since the 1500s.
+                       </p>
+                       <div class="flex justify-between mt-2">
+                           <span class="font-bold">Salary</span>
+                           <span class="font-bold">Location</span>
+                       </div>
+                        <button class="mt-4 bg-slate-800 text-white py-2 px-4 rounded hover:bg-slate-900">Read more</button>
+                   </div>
+               </div>
+           </div>
+            
+            <div class="bg-slate-600 col-span-5 row-span-3 flex flex-col rounded-tl-3xl rounded-bl-3xl rounded-r-md shadow-slate-900 drop-shadow-sm hover:drop-shadow-md">
+                 <div class="flex h-full">
+                    <img src="{{asset('images/logo3.jpg')}}" class="w-1/4 h-full object-cover rounded-tl-3xl rounded-bl-3xl" alt="Photo">
+                    <div class="ml-4 flex-1 p-8">
+                        <h2 class="text-xl font-bold mb-4 text-cyan-300">Title Two</h2>
+                        <p>
                             This is a small paragraph below the title.
                             Lorem is simply dummy text of the printing and typesetting industry. 
                             Lorem Ipsum has been the industry's standard dummy text ever 
-                            since the 1500s, when an unknown printer took a galley of type
-                            and scrambled it to make a type specimen book
+                            
                         </p>
-                        <div class="flex justify-between mt-2">
-                            <span class="text-gray-600">Salary</span>
-                            <span class="text-gray-600">Location</span>
+                        <div class="flex justify-between mt-4">
+                            <span class="font-bold">Salary</span>
+                            <span class="font-bold">Location</span>
                         </div>
-                         <button class="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">Button</button>
+                         <button class="mt-4 bg-slate-800 text-white py-2 px-4 rounded hover:bg-slate-900">Read more</button>
                     </div>
                 </div>
             </div>
 
-            <div class="bg-white col-span-2 row-span-6"></div>
-            <div class="bg-white col-span-5 row-span-3">Card one</div>
-            <div class="bg-white col-span-5 row-span-3">Card Two</div>
+            <aside class="bg-slate-600 col-span-2 row-span-6 rounded-md"></aside>
+
+            <div class="bg-slate-600 col-span-5 row-span-3 flex flex-col rounded-tl-3xl rounded-bl-3xl rounded-r-md shadow-slate-900 drop-shadow-sm hover:drop-shadow-md">
+                <div class="flex h-full">
+                   <img src="{{asset('images/logo2.png')}}" class="w-1/4 h-full object-cover rounded-tl-3xl rounded-bl-3xl" alt="Photo">
+                   <div class="ml-4 flex-1 p-8">
+                       <h2 class="text-xl font-bold mb-4 text-cyan-300">Title Three</h2>
+                       <p>
+                           This is a small paragraph below the title.
+                           Lorem is simply dummy text of the printing and typesetting industry. 
+                           Lorem Ipsum has been the industry's standard dummy text ever 
+                           
+                       </p>
+                       <div class="flex justify-between mt-4">
+                           <span class="font-bold">Salary</span>
+                           <span class="font-bold">Location</span>
+                       </div>
+                        <button class="mt-4 bg-slate-800 text-white py-2 px-4 rounded hover:bg-slate-900">Read more</button>
+                   </div>
+               </div>
+           </div>
+
+           <div class="bg-slate-600 col-span-5 row-span-3 flex flex-col rounded-tl-3xl rounded-bl-3xl rounded-r-md shadow-slate-900 drop-shadow-sm hover:drop-shadow-md">
+            <div class="flex h-full">
+               <img src="{{asset('images/logo4.jpg')}}" class="w-1/4 h-full object-cover rounded-tl-3xl rounded-bl-3xl" alt="Photo">
+               <div class="ml-4 flex-1 p-8">
+                   <h2 class="text-xl font-bold mb-4 text-cyan-300">Title Four</h2>
+                   <p>
+                       This is a small paragraph below the title.
+                       Lorem is simply dummy text of the printing and typesetting industry. 
+                       Lorem Ipsum has been the industry's standard dummy text ever 
+                       
+                   </p>
+                   <div class="flex justify-between mt-4">
+                       <span class="font-bold">Salary</span>
+                       <span class="font-bold">Location</span>
+                   </div>
+                    <button class="mt-4 bg-slate-800 text-white py-2 px-4 rounded hover:bg-slate-900">Read more</button>
+               </div>
+           </div>
+       </div>
+            
         </div>
 
         <div class="grid grid-cols-3 grid-height max-w-7xl gap-4 mx-auto mb-6">
-            <div class="bg-white col-span-3"></div>
+            <div class="bg-slate-600 col-span-3"></div>
         </div>
     </body>
 </html>
