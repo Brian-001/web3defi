ce9ca89f5e51fcc214a21c42821b0e91b9ac0717
Brian <karanjabrian001@gmail.com>
2024-11-15 00:00:54 +0300

    Added image upload input

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -7,22 +7,38 @@
 
         <h1 class="text-2xl text-cyan-300 mb-4 place-items-center">Create a new job</h1>
         {{-- Listing Title --}}
+
+        {{-- Flash message for success --}}
+        @if (session('success'))
+            <div class="mt-4 bg-green-200 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+                {{ session('success')}}
+            </div>
+        @endif
         <div class="mb-4 flex flex-col">
             <label for="listing_title" class="font-semibold">Listing Title <span class="text-sm text-red-500">*</span></label>
-            <input type="text" name="listing_title" id="listing_title" class="bg-slate-600 font-semibold rounded-lg p-2" required>
+            <input type="text" name="listing_title" id="listing_title" value="{{old('listing_title')}}" class="bg-slate-600 font-semibold rounded-lg p-2" required>
+            @error('listing_title')
+                <span class="text-red-500 text-sm">{{$message}}</span>
+            @enderror
         </div>
 
         <div class="flex flex-wrap">
             {{-- Company Description --}}
             <div class="mb-4 flex flex-col mr-4 w-full">
                 <label for="company_description" class="font-semibold">Company Description</label>
-                <textarea name="company_description" id="company_description" cols="30" rows="5" placeholder="Summarised information about the company" class="bg-slate-600 font-semibold rounded-lg h-32 resize-none p-2"></textarea>
+                <textarea name="company_description" id="company_description" cols="30" rows="5" value="{{old('company_description')}}" placeholder="Summarised information about the company" class="bg-slate-600 font-semibold rounded-lg h-32 resize-none p-2"></textarea>
+                @error('company_description')
+                    <span class="text-red-500 text-sm">{{$message}}</span>
+                @enderror
             </div>
 
             {{-- Job Description --}}
             <div class="mb-4 flex flex-col w-full">
                 <label for="job_description" class="font-semibold">Job Description<span class="text-sm text-red-500">*</span></label>
-                <textarea name="job_description" id="job_description" cols="30" rows="5" placeholder="Enter job description" class="bg-slate-600 font-semibold rounded-lg h-32 resize-none p-2"></textarea>
+                <textarea name="job_description" id="job_description" cols="30" rows="5" value="{{old('job_description')}}" placeholder="Enter job description" class="bg-slate-600 font-semibold rounded-lg h-32 resize-none p-2"></textarea>
+                @error('job_description')
+                    <span class="text-red-500 text-sm">{{$message}}</span>
+                @enderror
             </div>
         </div>
 
@@ -30,13 +46,19 @@
             {{-- Job Roles --}}
             <div class="mb-4 flex flex-col mr-4 w-full">
                 <label for="job_roles" class="font-semibold">Job Roles<span class="text-sm text-red-500">*</span></label>
-                <textarea name="job_roles" id="job_roles" cols="30" rows="5" placeholder="Enter roles and responsibilities" class="bg-slate-600 font-semibold rounded-lg h-32 resize-none p-2"></textarea>
+                <textarea name="job_roles" id="job_roles" cols="30" rows="5" value={{old('job_roles')}} placeholder="Enter roles and responsibilities" class="bg-slate-600 font-semibold rounded-lg h-32 resize-none p-2"></textarea>
+                @error('job_roles')
+                    <span class="text-red-500 text-sm">{{$message}}</span>
+                @enderror
             </div>
             
             {{-- Additional Information --}}
             <div class="mb-4 flex flex-col w-full">
                 <label for="additional_info" class="font-semibold">Additional Information</label>
-                <textarea name="additional_info" id="additional_info" cols="30" rows="5" placeholder="More info about job e.g., Benefits, Holidays" class="bg-slate-600 font-semibold rounded-lg h-32 resize-none p-2"></textarea>
+                <textarea name="additional_info" id="additional_info" cols="30" rows="5" value={{old('additional_info')}} placeholder="More info about job e.g., Benefits, Holidays" class="bg-slate-600 font-semibold rounded-lg h-32 resize-none p-2"></textarea>
+                @error('additional_info')
+                    <span class="text-red-500 text-sm">{{$message}}</span>
+                @enderror
             </div>
         </div>
 
@@ -48,12 +70,18 @@
                     {{-- Minimum Salary --}}
                     <div class="flex flex-col w-full"> {{-- Use w-full to ensure full width on small screens --}}
                         <label for="min_salary" class="text-sm">Minimum Salary<span class="text-sm text-red-500">*</span></label>
-                        <input type="number" name="min_salary" id="min_salary" min=0 class="bg-slate-600 font-semibold rounded-lg p-2" required>
+                        <input type="number" name="min_salary" id="min_salary" min=0 value="{{old('min_salary')}}" class="bg-slate-600 font-semibold rounded-lg p-2" required>
+                        @error('min_salary')
+                            <span class="text-red-500 text-sm">{{$message}}</span>
+                        @enderror
                     </div>
                     {{-- Maximum Salary --}}
                     <div class="flex flex-col w-full"> {{-- Use w-full to ensure full width on small screens --}}
                         <label for="max_salary" class="text-sm">Maximum Salary<span class="text-sm text-red-500">*</span></label>
-                        <input type="number" name="max_salary" min="0" id="max_salary" class="bg-slate-600 font-semibold rounded-lg p-2" required>
+                        <input type="number" name="max_salary" min="0" id="max_salary" value="{{old('max_salary')}}" class="bg-slate-600 font-semibold rounded-lg p-2" required>
+                        @error('max_salary')
+                            <span class="text-red-500 text-sm">{{$message}}</span>
+                        @enderror
                     </div>
                 </div>
             </div>
@@ -63,7 +91,10 @@
         {{-- Location --}}
         <div class="mb-4 flex flex-col">
             <label for="location" class="font-semibold">Location<span class="text-sm text-red-500">*</span></label>
-            <input type="text" name="location" id="location" placeholder="City, Country" class="bg-slate-600 font-semibold rounded-lg p-2">
+            <input type="text" name="location" id="location" value="{{old('location')}}" placeholder="City, Country" class="bg-slate-600 font-semibold rounded-lg p-2">
+            @error('location')
+                <span class="text-red-500 text-sm">{{$message}}</span>
+            @enderror
         </div>
 
         <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -71,28 +102,42 @@
             <div class="mb-4 flex flex-col">
                 <label for="tags" class="font-semibold">Tags<span class="text-sm text-red-500">*</span></label>
                 <select name="tags" id="tags" class="bg-slate-600 font-semibold rounded-lg p-2">
-                    <option value="">Select a tag</option>
+                    <option value="" class="text-gray-200" disabled>--Select a tag--</option>
                     <option value="Laravel">Laravel</option>
                     <option value="JavaScript">JavaScript</option>
                     <option value="Java">Java</option>
                     <option value="Golang">Golang</option>
                     <option value="Rust">Rust</option>
                 </select>
+                @error('tags')
+                    <span class="text-red-500 text-sm">{{$message}}</span>
+                @enderror
             </div>
 
             {{-- Job Type --}}
             <div class="mb-4 flex flex-col">
                 <label for="job_type" class= "font-semibold">Job Type<span class="text-sm text-red-500">*</span></label> 
-                <select name= "job_type" id= "job_type"class= "bg-slate-600 font-semibold rounded-lg p-2">
+                <select name= "job_type" id= "job_type" value="{{old('job_type')}}" class= "bg-slate-600 font-semibold rounded-lg p-2">
+                    <option value="" class="text-gray-200" disabled>--Select Job Type--</option>
                     <option value= "onsite">On-site</option> 
                     <option value= "remote">Remote</option> 
                     <option value= "hybrid">Hybrid</option> 
-                </select> 
+                </select>
+                @error('job_type')
+                    <span class="text-red-500 text-sm">{{$message}}</span>
+                @enderror 
             </div> 
-            <div>
-                <button type="submit" class="bg-cyan-300 text-slate-700 px-2 py-1.5 rounded-md">Post Job</button>
-            </div>
+            
+        </div>
+        {{--image upload--}}
+        <div class="mb-4 flex flex-col">
+            <label for="listing_logo" class="font-semibold">Company Logo</label>
+            <input type="file" name="listing_logo" id="listing_logo" class="file:px-2 file:py-1.5 file:border-none file:rounded-lg file:bg-slate-600
+             file:text-white file:cursor-pointer rounded-md shadow-sm outline-none border-none file:outline-none shadow-cyan-300" accept="image/*">
         </div> 
+        <div>
+            <button type="submit" class="bg-cyan-300 text-slate-700 px-2 py-1.5 rounded-md">Post Job</button>
+        </div>
     </form> 
 </div> 
 @endsection
