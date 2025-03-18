8ae3e4c9dfa52d2a769c30cfb4804ebf40feb95e
Brian <karanjabrian001@gmail.com>
2025-03-18 14:20:53 +0300

    Added role User relationship in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,7 +64,11 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
-
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+    
     public function hasRole($roleName)
     {
         return $this->role->name === $roleName;
